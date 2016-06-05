312a2afe4c98f80e4603bf6fc0027de4637e4860
geomaas <geoff.maas@gmail.com>
2016-06-05 18:12:13 -0400

    sunday afternoon

--- data.js
+++ data.js
@@ -1,61 +1,82 @@
 var albumGroup = [
   {
-    title: "Blue Angels",
-    cover: "album\ list/album\ 1/dsc_0082_4537140972_o.jpg",
+    title: "Blue-Angels",
+    cover: "albumlist/album1/dsc_0082_4537140972_o.jpg",
     pictures: [
 
-      {pic: "album\ list/album\ 1/dsc_0076_4537140524_o.jpg"},
-      {pic: "album\ list/album\ 1/dsc_0082_4537140972_o.jpg"},
-      {pic: "album\ list/album\ 1/dsc_0098_4536508433_o.jpg"},
-      {pic: "album\ list/album\ 1/dsc_0114_4537141444_o.jpg"},
-      {pic: "album\ list/album\ 1/dsc_0145_4537141658_o.jpg"},
-      {pic: "album\ list/album\ 1/dsc_0164_4536511151_o.jpg"},
+      {pic: "albumlist/album1/dsc_0076_4537140524_o.jpg"},
+      {pic: "albumlist/album1/dsc_0082_4537140972_o.jpg"},
+      {pic: "albumlist/album1/dsc_0098_4536508433_o.jpg"},
+      {pic: "albumlist/album1/dsc_0114_4537141444_o.jpg"},
+      {pic: "albumlist/album1/dsc_0145_4537141658_o.jpg"},
+      {pic: "albumlist/album1/dsc_0164_4536511151_o.jpg"},
 
     ]
   },
   {
     title: "Artwork",
-    cover: "album\ list/album\ 2/img_2459jpg_3456532555_o.jpg",
+    cover: "albumlist/album2/img_2459jpg_3456532555_o.jpg",
     pictures: [
-      {
 
-      }
+      {pic: "albumlist/album2/img_2436jpg_3457343096_o.jpg"},
+      {pic: "albumlist/album2/img_2438jpg_3457345618_o.jpg"},
+      {pic: "albumlist/album2/img_2447jpg_3456529219_o.jpg"},
+      {pic: "albumlist/album2/img_2457jpg_3457348934_o.jpg"},
+      {pic: "albumlist/album2/img_2459jpg_3456532555_o.jpg"},
+      {pic: "albumlist/album2/img_3027_3457403544_o.jpg"},
+
     ]
   },
   {
-    title: "Charleston Storms",
-    cover: "album\ list/album\ 3/dsc_0066_6191043872_o.jpg",
+    title: "Charleston-Storms",
+    cover: "albumlist/album3/dsc_0066_6191043872_o.jpg",
     pictures: [
-      {
 
-      }
+      {pic: "albumlist/album3/dsc_0028_6072385880_o.jpg"},
+      {pic: "albumlist/album3/dsc_0066_6191043872_o.jpg"},
+      {pic: "albumlist/album3/dsc_0075_6191044100_o.jpg"},
+      {pic: "albumlist/album3/dsc_0356_6027737277_o.jpg"},
+      {pic: "albumlist/album3/dsc_0369_6027737453_o.jpg"},
+      {pic: "albumlist/album3/dsc_0379_6027737889_o.jpg"},
+
     ]
   },
   {
-    title: "St. Petersburg",
-    cover: "album\ list/album\ 4/6026678188_d26999225e_b.jpg",
+    title: "Russia",
+    cover: "albumlist/album4/6026678188_d26999225e_b.jpg",
     pictures: [
-      {
 
-      }
+      {pic: "albumlist/album4/6026122015_5fec95109f_b.jpg"},
+      {pic: "albumlist/album4/6026124681_8d0dd978e4_b.jpg"},
+      {pic: "albumlist/album4/6026135975_f84c3e9c51_b.jpg"},
+      {pic: "albumlist/album4/6026676090_484ae4368b_b.jpg"},
+      {pic: "albumlist/album4/6026677736_111b22da8b_b.jpg"},
+      {pic: "albumlist/album4/6026678188_d26999225e_b.jpg"},
     ]
   },
   {
-    title: "Jersey Surfing",
-    cover: "album\ list/album\ 5/5010185841_5665e03bd9_b.jpg",
+    title: "Surfing",
+    cover: "albumlist/album5/5010185841_5665e03bd9_b.jpg",
     pictures: [
-      {
-
-      }
+    {pic:"albumlist/album5/5010184023_26425b39ce_b.jpg"},
+    {pic:"albumlist/album5/5010184475_72fe13f4a6_b.jpg"},
+    {pic:"albumlist/album5/5010185841_5665e03bd9_b.jpg"},
+    {pic:"albumlist/album5/5010186591_c7462f18e0_b.jpg"},
+    {pic:"albumlist/album5/5010790354_a5d5b2d5a3_b.jpg"},
+    {pic:"albumlist/album5/5010791234_e2bbf4bab6_b.jpg"},
     ]
   },
   {
-    title: "Drone Photos",
-    cover: "album\ list/album\ 6/DJI_0054\ copy.JPG",
+    title: "Drone-Photos",
+    cover: "albumlist/album6/DJI_0054\ copy.JPG",
     pictures: [
-      {
 
-      }
+      {pic:"albumlist/album6/DJI_0002 copy.JPG"},
+      {pic:"albumlist/album6/DJI_0003 copy.JPG"},
+      {pic:"albumlist/album6/DJI_0010 copy.JPG"},
+      {pic:"albumlist/album6/DJI_00021copy.JPG"},
+      {pic:"albumlist/album6/DJI_0054 copy.JPG"},
+      {pic:"albumlist/album6/holycitycontainer.JPG"},
     ]
   },
 
--- main.js
+++ main.js
@@ -13,28 +13,189 @@ albumGroup.forEach(function(element,idx) {
 $('.main-container').append(albumListStr);
 
 
-// photo index js
-// // debugger
-// var photoListStr = '<div class="row">';
-// albumGroup[0].pictures.forEach(function(element,idx) {
-//     // console.log("THIS IS ME", element)
-//   photoListStr += `
+// blue- angels album click event
+
+$(".albums").on("click",function(event){
+  event.preventDefault();
+var photoListStr = '<div class="row">';
+albumGroup[0].pictures.forEach(function(element,idx) {
+    console.log("THIS IS ME", element)
+  photoListStr += `
+  <div class="thumbnail">
+  <img src="${element.pic}" alt="" />
+  <h3></h3>
+  </div>
+  `
+  // debugger
+  $(".album-container").removeClass("hidden")
+  $(".album-selector").removeClass("hidden")
+  $('.main-container').removeClass('active').addClass('hidden');
+  $('header').removeClass('active').addClass('hidden');
+  // console.log(photoListStr);
+});
+$('.album-container').append(photoListStr);
+});
+
+
+// artwork click event
+
+$(".albums").on("click",function(event){
+var photoListStr = '<div class="row">';
+albumGroup[1].pictures.forEach(function(element,idx) {
+    // console.log("THIS IS ME", element)
+  photoListStr += `
+  <div class="thumbnail">
+  <img src="${element.pic}" alt="" />
+  <h3></h3>
+  </div>
+  `
+  // debugger
+  $(".album-container").removeClass("hidden")
+  $(".album-selector").removeClass("hidden")
+  $('.main-container').removeClass('active').addClass('hidden');
+  // console.log(photoListStr);
+});
+$('.album-container').append(photoListStr);
+});
+
+
+// storms click event
+
+
+$(".albums").on("click",function(event){
+var photoListStr = '<div class="row">';
+albumGroup[2].pictures.forEach(function(element,idx) {
+    // console.log("THIS IS ME", element)
+  photoListStr += `
+  <div class="thumbnail">
+  <img src="${element.pic}" alt="" />
+  <h3></h3>
+  </div>
+  `
+  // debugger
+  $(".album-container").removeClass("hidden")
+  $(".album-selector").removeClass("hidden")
+  $('.main-container').removeClass('active').addClass('hidden');
+  // console.log(photoListStr);
+});
+$('.album-container').append(photoListStr);
+});
+
+
+// russia click event
+
+$(".albums").on("click",function(event){
+var photoListStr = '<div class="row">';
+albumGroup[3].pictures.forEach(function(element,idx) {
+    // console.log("THIS IS ME", element)
+  photoListStr += `
+  <div class="thumbnail">
+  <img src="${element.pic}" alt="" />
+  <h3></h3>
+  </div>
+  `
+  // debugger
+  $(".album-container").removeClass("hidden")
+  $(".album-selector").removeClass("hidden")
+  $('.main-container').removeClass('active').addClass('hidden');
+  // console.log(photoListStr);
+});
+$('.album-container').append(photoListStr);
+});
+
+
+//surfing click event
+
+$(".albums").on("click",function(event){
+var photoListStr = '<div class="row">';
+albumGroup[4].pictures.forEach(function(element,idx) {
+    // console.log("THIS IS ME", element)
+  photoListStr += `
+  <div class="thumbnail">
+  <img src="${element.pic}" alt="" />
+  <h3></h3>
+  </div>
+  `
+  // debugger
+  $(".album-container").removeClass("hidden")
+  $(".album-selector").removeClass("hidden")
+  $('.main-container').removeClass('active').addClass('hidden');
+  // console.log(photoListStr);
+});
+$('.album-container').append(photoListStr);
+});
+
+
+//drone click event
+
+$(".albums").on("click",function(event){
+  event.preventDefault();
+var photoListStr = '<div class="row">';
+albumGroup[5].pictures.forEach(function(element,idx) {
+    // console.log("THIS IS ME", element)
+  photoListStr += `
+  <div class="thumbnail">
+  <img src="${element.pic}" alt="" />
+  <h3></h3>
+  </div>
+  `
+  // debugger
+  $(".album-container").removeClass("hidden")
+  $(".album-selector").removeClass("hidden")
+  $('.main-container').removeClass('active').addClass('hidden');
+  // console.log(photoListStr);
+});
+$('.album-container').append(photoListStr);
+});
+
+
+// large photo click event
+
+$(".thumbnail").on("click",function(event){
+  event.preventDefault();
+  var clicked = $(this).find('img');
+
+  console.log("this is me",clicked);
+  });
+  // var photStr = '<div class="photo-title">';
+//   photoStr += `
 //   <div class="thumbnail">
 //   <img src="${element.pic}" alt="" />
 //   <h3></h3>
 //   </div>
 //   `
-// });
+//   // debugger
+//   // $(".album-container").removeClass("hidden")
+//   // $(".album-selector").removeClass("hidden")
+//   // $('.photo-container').removeClass('active').addClass('hidden');
+//   console.log(photoListStr);
+
+// $('.photo-container').append(photoStr);
+
+
+
+// $('.albums').on('click',function(event) {
+//   event.preventDefault();
 //
-// $('.album-container').append(photoListStr);
-
-
-$('.albums').on('click',function(event) {
-
-
-  var clicked = $(this).find('h3').text()
-  window.curr = albumGroup.filter(function(element) {
-     return element.title === clicked;
-
-  });
-});
+//   var clicked = $(this).find('h3').text()
+//   window.curr = albumGroup.filter(function(element) {
+//     // console.log(clicked);
+//     //  return element.title === clicked;
+//        var ourClassToShow = "." + clicked.toLowerCase();
+//        $(ourClassToShow).removeClass('hidden');
+//        $('main-container').removeClass('active').addClass('hidden');
+//     //  console.log(ourClassToShow);
+//
+//        var photoListStr = '<div class="row">';
+//        albumGroup.forEach(function(element,idx) {
+//          photoListStr += `
+//          <div class="albums">
+//          <img src="${element.pictures}" alt="" />
+//          <h3></h3>
+//          </div>
+//        `
+// console.log(photoListStr);
+//      });
+//
+//   });
+// });
